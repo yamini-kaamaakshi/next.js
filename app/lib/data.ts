c08515d3ea4b38448b46143e21df0c40eed3bbc1
Yamini <yaminikaamaakshi@gmail.com>
2025-03-04 17:43:24 +0530

    fechdata

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -68,6 +68,7 @@ export async function fetchCardData() {
       invoiceStatusPromise,
     ]);
 
+
     const numberOfInvoices = Number(data[0][0].count ?? '0');
     const numberOfCustomers = Number(data[1][0].count ?? '0');
     const totalPaidInvoices = formatCurrency(data[2][0].paid ?? '0');
